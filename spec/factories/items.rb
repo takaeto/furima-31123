6fba238171ceeabf82eb3b651e17d925ec6de609
takaeto <taka.etoile@gmail.com>
2020-11-06 00:21:33 +0900

    モデルのテストコード実施

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    name { "仕事服" }
+    description { "スーツ" }
+    category_id { '2' }
+    condition_id { '2' }
+    postage_payer_id { '2' }
+    prefecture_id { '2' }
+    ship_date_id { '2' }
+    price { '1000' }
+    association :user
   end
 end
