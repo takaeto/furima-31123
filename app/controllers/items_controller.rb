f0a39206eb22958cf0ca554d35fe89bc5a895d96
takaeto <taka.etoile@gmail.com>
2020-11-06 12:13:11 +0900

    newでインスタンスの生成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,7 +9,8 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.create(item_params)
+    @item = Item.new(item_params)
+    @item.save
     if @item.save
       redirect_to root_path
     else 
